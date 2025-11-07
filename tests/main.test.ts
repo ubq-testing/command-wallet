0b754b01dacaf2c01cfe1a85fd4fc1f8cdf870cc
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-11-07 12:23:17 +0000

    test: update wallet command tests to handle wallet unset and set commands

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -27,7 +27,7 @@ const eventName = "issue_comment.created";
 jest.unstable_mockModule("ethers", () => ({
   ethers: {
     JsonRpcProvider: jest.fn(() => ({
-      resolveName: jest.fn(async () => "0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd"),
+      resolveName: jest.fn(async () => "0x0000000000000000000000000000000000000001"),
     })),
     getAddress: (jest.requireActual("ethers") as typeof ethers).getAddress,
   },
@@ -50,81 +50,10 @@ describe("Wallet command tests", () => {
 
   it("Should handle /wallet comment", async () => {
     const spy = jest.spyOn(Logs.prototype, "ok");
-    const context = {
-      eventName: eventName,
-      config: { registerWalletWithVerification: false },
-      payload: {
-        ...commentCreatedPayload,
-        comment: {
-          ...commentCreatedPayload.comment,
-          body: "/wallet ubiquibot.eth",
-        },
-      },
-      command: {
-        name: "wallet",
-        parameters: {
-          walletAddress: "ubiquibot.eth",
-        },
-      },
-      octokit: new Octokit(),
-      env: {
-        SUPABASE_URL: process.env.SUPABASE_URL,
-        SUPABASE_KEY: process.env.SUPABASE_KEY,
-      },
-      logger: new Logs("debug"),
-      commentHandler: new CommentHandler(),
-    } as unknown as Context;
-    await plugin(context);
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenLastCalledWith(
-      "Successfully set wallet",
-      expect.objectContaining({
-        address: "0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd",
-        sender: "ubiquibot",
-      })
-    );
-  }, 20000);
 
-  it("Should handle wallet command", async () => {
-    const spy = jest.spyOn(Logs.prototype, "ok");
-    const context = {
-      eventName: eventName,
-      config: { registerWalletWithVerification: false },
-      payload: {
-        ...commentCreatedPayload,
-        comment: {
-          ...commentCreatedPayload.comment,
-          body: "@UbiquityOS set my wallet to ubiquibot.eth",
-        },
-      },
-      command: {
-        name: "wallet",
-        parameters: {
-          walletAddress: "ubiquibot.eth",
-        },
-      },
-      octokit: new Octokit(),
-      env: {
-        SUPABASE_URL: process.env.SUPABASE_URL,
-        SUPABASE_KEY: process.env.SUPABASE_KEY,
-      },
-      logger: new Logs("debug"),
-      commentHandler: new CommentHandler(),
-    } as unknown as Context;
-    await plugin(context);
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenLastCalledWith(
-      "Successfully set wallet",
-      expect.objectContaining({
-        address: "0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd",
-        sender: "ubiquibot",
-      })
-    );
-  }, 20000);
+    // first we unset the wallet as the db is seeded with the wallet already set
 
-  it("Should unregister a wallet", async () => {
-    const spy = jest.spyOn(Logs.prototype, "ok");
-    await plugin({
+    const context = {
       eventName: eventName,
       config: { registerWalletWithVerification: false },
       payload: {
@@ -140,11 +69,82 @@ describe("Wallet command tests", () => {
         SUPABASE_URL: process.env.SUPABASE_URL,
         SUPABASE_KEY: process.env.SUPABASE_KEY,
       },
-      logger: new Logs("info"),
+      logger: new Logs("debug"),
       commentHandler: new CommentHandler(),
-    } as unknown as Context);
+    } as unknown as Context;
+    await plugin(context);
     expect(spy).toHaveBeenCalledTimes(1);
+
     expect(spy).toHaveBeenLastCalledWith("Successfully unset wallet");
+
+    context.payload.comment.body = "/wallet set my wallet to ubiquibot.eth";
+    context.command = {
+      name: "wallet",
+      parameters: {
+        walletAddress: "ubiquibot.eth",
+        unset: false,
+      },
+    };
+    await plugin(context);
+    expect(spy).toHaveBeenCalledTimes(2);
+
+    expect(spy).toHaveBeenLastCalledWith(
+      "Successfully set wallet",
+      expect.objectContaining({
+        address: "0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd",
+        sender: "ubiquibot",
+      })
+    );
+  }, 20000);
+
+  it("Should handle wallet command", async () => {
+    const spy = jest.spyOn(Logs.prototype, "ok");
+
+    // first we unset the wallet as the db is seeded with the wallet already set
+
+    const context = {
+      eventName: eventName,
+      config: { registerWalletWithVerification: false },
+      payload: {
+        ...commentCreatedPayload,
+        comment: {
+          ...commentCreatedPayload.comment,
+          body: "/wallet unset",
+        },
+      },
+      command: null,
+      octokit: new Octokit(),
+      env: {
+        SUPABASE_URL: process.env.SUPABASE_URL,
+        SUPABASE_KEY: process.env.SUPABASE_KEY,
+      },
+      logger: new Logs("debug"),
+      commentHandler: new CommentHandler(),
+    } as unknown as Context;
+    await plugin(context);
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    expect(spy).toHaveBeenLastCalledWith("Successfully unset wallet");
+
+    // Now set the wallet
+    context.payload.comment.body = "@UbiquityOS set my wallet to ubiquibot.eth";
+    context.command = {
+      name: "wallet",
+      parameters: {
+        walletAddress: "ubiquibot.eth",
+        unset: false,
+      },
+    };
+    await plugin(context);
+    expect(spy).toHaveBeenCalledTimes(2);
+
+    expect(spy).toHaveBeenLastCalledWith(
+      "Successfully set wallet",
+      expect.objectContaining({
+        address: "0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd",
+        sender: "ubiquibot",
+      })
+    );
   }, 20000);
 
   it("should warn if the wallet is already registered to the user", async () => {
@@ -156,13 +156,13 @@ describe("Wallet command tests", () => {
         ...commentCreatedPayload,
         comment: {
           ...commentCreatedPayload.comment,
-          body: "/wallet 0x2D1D986EbF1f8dE65A1Fe876053c0b81cB5Fb49e",
+          body: "/wallet 0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd",
         },
       },
       command: {
         name: "wallet",
         parameters: {
-          walletAddress: "0x2D1D986EbF1f8dE65A1Fe876053c0b81cB5Fb49e",
+          walletAddress: "0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd",
         },
       },
       octokit: new Octokit(),
